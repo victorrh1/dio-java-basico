c425782997549f689f701a25d12db561351990bd
victorrh1 <victorwow7@outlook.com>
2025-03-04 15:05:29 -0300

    ClassName

---
+++ meu-primeiro-projeto-java/src/ClassName.java
@@ -0,0 +1,16 @@
+public class ClassName {
+
+    public static void main (String [] args) {
+
+        String primeiroNome = "José";
+        String segundoNome = "Victor";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+}
