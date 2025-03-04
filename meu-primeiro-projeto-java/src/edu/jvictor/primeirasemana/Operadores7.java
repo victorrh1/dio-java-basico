aee4c9c506a3857bda351356653a1b75c8debf0d
victorrh1 <victorwow7@outlook.com>
2025-03-04 18:52:32 -0300

    Comparar operadores relacionais

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores7.java
@@ -0,0 +1,17 @@
+package edu.jvictor.primeirasemana;
+
+public class Operadores7 {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+    }
+}
