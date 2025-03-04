0e6d0f29a75c07614b09b8e0f27fac4d2ff15126
victorrh1 <victorwow7@outlook.com>
2025-03-04 17:52:42 -0300

    Variações de Operadores

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores3.java
@@ -0,0 +1,15 @@
+package edu.jvictor.primeirasemana;
+
+public class Operadores3 {
+    public static void main(String[] args) {
+     
+        
+        int numero = 5;
+
+        numero = numero + 1;
+        System.out.println(numero);
+
+        numero++;
+        System.out.println(numero);
+    }
+}
