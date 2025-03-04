554543d63b20c16c97f0b26a2132aa435bbee18a
victorrh1 <victorwow7@outlook.com>
2025-03-04 17:54:54 -0300

    Conversão de um boleano

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores4.java
@@ -0,0 +1,10 @@
+package edu.jvictor.primeirasemana;
+
+public class Operadores4 {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel;
+        System.out.println(variavel);
+    }
+}
