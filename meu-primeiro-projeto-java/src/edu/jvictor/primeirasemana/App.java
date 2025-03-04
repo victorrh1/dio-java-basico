a3734aef53eafefecfb273e4d182cf86e5d68d0e
victorrh1 <victorwow7@outlook.com>
2025-03-04 16:20:12 -0300

    Update

--- meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/App.java
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/App.java
@@ -1,3 +1,4 @@
+package edu.jvictor.primeirasemana;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas Vindas Turma DIO");
--- meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/BoletimEstudantil.java
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.jvictor.primeirasemana;
 public class BoletimEstudantil {
 
     public static void main(String[] args) {
