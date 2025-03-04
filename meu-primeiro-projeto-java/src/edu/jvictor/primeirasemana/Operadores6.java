3b7c626825e93340518243ef7d1d380cce664be5
victorrh1 <victorwow7@outlook.com>
2025-03-04 18:09:39 -0300

    Outra variação de comparação A e B

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores6.java
@@ -0,0 +1,15 @@
+package edu.jvictor.primeirasemana;
+
+public class Operadores6 {
+    public static void main(String[] args) {
+     
+        int a, b;
+        
+        a = 6;
+        b = 6;
+        
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+    }
+    
+}
