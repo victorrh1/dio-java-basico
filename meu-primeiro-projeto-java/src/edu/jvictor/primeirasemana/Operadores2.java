14525b7eae601b94345cae94ea58c964739416ae
victorrh1 <victorwow7@outlook.com>
2025-03-04 17:34:47 -0300

    Negativo e positivo

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores2.java
@@ -0,0 +1,15 @@
+package edu.jvictor.primeirasemana;
+
+class Operadores2 {
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+        System.out.println("Mostra o numero negativo: " + numero);
+
+        numero = numero * -1;
+        System.out.println("Mostra o numero positivo: " + numero);
+
+    }
+}
