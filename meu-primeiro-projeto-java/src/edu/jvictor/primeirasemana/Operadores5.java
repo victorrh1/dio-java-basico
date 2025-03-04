ee41a98c5d8abdc199f695c81e7e283e313ee0a8
victorrh1 <victorwow7@outlook.com>
2025-03-04 18:02:57 -0300

    Comparar valores inteiros A e B

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/primeirasemana/Operadores5.java
@@ -0,0 +1,17 @@
+package edu.jvictor.primeirasemana;
+
+public class Operadores5 {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = "";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+    }
+}
