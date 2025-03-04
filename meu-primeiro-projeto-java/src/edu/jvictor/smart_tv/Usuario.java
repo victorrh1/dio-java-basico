3ffb3e61bafcdfcbb49f99485ebfb1c94fb75d0c
victorrh1 <victorwow7@outlook.com>
2025-03-04 20:12:43 -0300

    Sistema_Smart_TV

---
+++ meu-primeiro-projeto-java/src/edu/jvictor/smart_tv/Usuario.java
@@ -0,0 +1,24 @@
+package edu.jvictor.smart_tv;
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+    }
+}
