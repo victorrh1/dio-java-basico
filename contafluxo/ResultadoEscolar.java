27470a860931d6e8c96c32d01d7cd813252d190b
victorrh1 <victorwow7@outlook.com>
2025-03-16 03:47:48 -0300

    Update

---
+++ contafluxo/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+package contafluxo;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+
+        else
+        System.out.println("Reprovado");
+    }
+}
--- contafluxo/src/CaixaEletronico.java
+++ contafluxo/src/CaixaEletronico.java
@@ -4,9 +4,12 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
 
-            System.out.println(saldo);
+        }else
+            System.out.println("Saldo insuficiente");
+            
     }
 }
