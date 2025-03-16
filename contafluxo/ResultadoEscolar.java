7b8120f7dfcc2a3924a7e8c0195a8c6ae6be68a4
victorrh1 <victorwow7@outlook.com>
2025-03-16 03:59:03 -0300

    update

--- contafluxo/ResultadoEscolar.java
+++ contafluxo/ResultadoEscolar.java
@@ -2,10 +2,13 @@ package contafluxo;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 5;
 
         if(nota >= 7)
             System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de Recuperação");
 
         else
         System.out.println("Reprovado");
